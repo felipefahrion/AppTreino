39681d26e235a0a99d241e146713ca7530a88f13
Felipe Fahrion <felipefahrion@POATEKMBP-Felipe-Fahrion.local>
2024-01-24 11:19:24 -0300

    add diet data

---
+++ src/data-diet.js
@@ -0,0 +1,8 @@
+export const dataDiet = [
+    { meal: 1, food: ['200 G DE MAMÃO OU UVAS', '20 G DE WHEY', '100 ML DE IOGURTE NATURAL'], time: "CAFÉ DA MANHÃ / PRÉ TREINO" },
+    { meal: 2, food: ['200 G DE MAMÃO OU UVAS', '20 G DE WHEY', '100 ML DE IOGURTE NATURAL'], time: "CAFÉ DA MANHÃ / PRÉ TREINO" },
+    { meal: 3, food: ['200 G DE MAMÃO OU UVAS', '20 G DE WHEY', '100 ML DE IOGURTE NATURAL'], time: "CAFÉ DA MANHÃ / PRÉ TREINO" },
+    { meal: 4, food: ['200 G DE MAMÃO OU UVAS', '20 G DE WHEY', '100 ML DE IOGURTE NATURAL'], time: "CAFÉ DA MANHÃ / PRÉ TREINO" },
+    { meal: 5, food: ['200 G DE MAMÃO OU UVAS', '20 G DE WHEY', '100 ML DE IOGURTE NATURAL'], time: "CAFÉ DA MANHÃ / PRÉ TREINO" },
+    { meal: 6, food: ['200 G DE MAMÃO OU UVAS', '20 G DE WHEY', '100 ML DE IOGURTE NATURAL'], time: "CAFÉ DA MANHÃ / PRÉ TREINO" },
+]
